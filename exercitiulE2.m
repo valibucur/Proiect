bc33678a6c3c4c433bdfa01cfeea247c6433b0e2
Bucur Valeriu <bucurv95@gmail.com>
2016-11-05 20:05:00 +0200

    Add files via upload

---
+++ exercitiulE2.m
@@ -0,0 +1,3 @@
+%vector elemente aleatoare cu distrib. gauss
+v= randn(1,5)
+u = v(v<=0) %afisare elemente negative
---
+++ exercitiulE3.m
@@ -0,0 +1,5 @@
+z = [ complex(2,3) complex(3,1) complex(4,4)]
+a=real (z)
+mean (a) %media aritmetica a partii reale a lui z
+x= z.^2
+q = z* transpose (z) %înmul?irea vectorului ini?ial cu transpusul s?u
