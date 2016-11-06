3713b9f6269ef66049d3f2d6f0a64e80e36fcb39
Bucur Valeriu <bucurv95@gmail.com>
2016-11-06 21:07:07 +0200

    Add files via upload

---
+++ T1_Ex1_BucurValeriu.m
@@ -0,0 +1,21 @@
+function exercitiu1_tema()
+
+fs = 5000;
+timp = 0:1/fs:100;
+
+x = square(pi*timp,25);
+
+for i = 1:1:length(x)
+   if x(i) > 0
+       x(i) = x(i)/2;
+   end
+end
+
+plot(timp,x)
+
+axis([0 6 -1.5 1.5])
+xlabel('Timp (sec)')
+ylabel('Amplitudine')
+title('Semnal dreptunghiular')
+
+end
